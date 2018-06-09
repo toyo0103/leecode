910c16a320fc20577d5774f01dd18f86c33323f0
StevenTsai <steventsai@urtoday.com>
2018-06-09 20:06:13 +0800

    【LeetCode】Serialize and Deserialize BST

---
+++ LeetCode/449.SerializeAndDeserializeBST.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+    //Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be serialized to a string and this string can be deserialized to the original tree structure.
+    //The encoded string should be as compact as possible.
+    //Note: Do not use class member/global/static variables to store states.Your serialize and deserialize algorithms should be stateless.
+
+    public class TreeNode
+    {
+         public int val;
+         public TreeNode left;
+         public TreeNode right;
+         public TreeNode(int x) { val = x; }
+     }
+
+    public class Codec
+    {
+        public string serialize(TreeNode root)
+        {
+            if (root == null)
+            {
+                return "#!";
+            }
+
+            string res = root.val + "!";
+            res += serialize(root.left);
+            res += serialize(root.right);
+            return res;
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(string data)
+        {
+            var nodeData = data.Split(new string[] { "!" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var queue = new Queue<string>();
+
+            foreach (var node in nodeData)
+            {
+                queue.Enqueue(node);
+            }
+
+            return RebuildNode(queue);
+        }
+
+        private TreeNode RebuildNode(Queue<string> queue)
+        {
+            var Value = queue.Dequeue();
+
+            if (Value == "#")
+            {
+                return null;
+            }
+
+            TreeNode node = new TreeNode(int.Parse(Value));
+
+            node.left = RebuildNode(queue);
+            node.right = RebuildNode(queue);
+
+            return node;
+        }
+    }
+}
---
+++ LeetCodeTests/449.SerializeAndDeserializeBST.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class CodecTests
+    {
+        [TestMethod()]
+        public void serializeTest()
+        {
+            //arrange
+            var node = new TreeNode(1);
+            var sut = new Codec();
+
+            var expected = "1!#!#!";
+
+            //act
+            var actual = sut.serialize(node);
+
+            //assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void serializeTest_傳入Null應回傳空字串()
+        {
+            //arrange
+            var sut = new Codec();
+
+            var expected = "#!";
+
+            //act
+            var actual = sut.serialize(null);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod()]
+        public void serializeTest_有左邊子節點()
+        {
+            //arrange
+            var node = new TreeNode(1);
+            node.left = new TreeNode(2);
+
+            var sut = new Codec();
+
+            var expected = "1!2!#!#!#!";
+
+            //act
+            var actual = sut.serialize(node);
+
+            //assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void serializeTest_有右邊子節點()
+        {
+            //arrange
+            var node = new TreeNode(1);
+            node.right = new TreeNode(2);
+
+            var sut = new Codec();
+
+            var expected = @"1!#!2!#!#!";
+
+            //act
+            var actual = sut.serialize(node);
+
+            //assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void serializeTest_三層節點()
+        {
+            //arrange
+            var node = new TreeNode(1);
+            node.left = new TreeNode(2);
+            node.right = new TreeNode(3);
+
+            node.left.left = new TreeNode(4);
+            node.right.right = new TreeNode(5);
+
+            var sut = new Codec();
+
+            var expected = "1!2!4!#!#!#!3!#!5!#!#!";
+
+            //act
+            var actual = sut.serialize(node);
+
+            //assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void deserializeTest()
+        {
+            //arrange
+            var nodeSerializeString = @"1!#!#!";
+            var sut = new Codec();
+            
+            var expected = new TreeNode(1);
+            //act
+            var actual = sut.deserialize(nodeSerializeString);
+
+            //assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void deserializeTest_Null_Node()
+        {
+            //arrange
+            var nodeSerializeString = "#!";
+            var sut = new Codec();
+
+            //act
+            var actual = sut.deserialize(nodeSerializeString);
+
+            //assert
+            actual.Should().BeNull();
+        }
+
+        [TestMethod()]
+        public void deserializeTest_有左邊子節點()
+        {
+            //arrange
+            var nodeSerializeString = @"1!4!#!#!#!";
+            var sut = new Codec();
+
+            var expected = new TreeNode(1);
+            expected.left = new TreeNode(4);
+            //act
+            var actual = sut.deserialize(nodeSerializeString);
+
+            //assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void deserializeTest_有右邊子節點()
+        {
+            //arrange
+            var nodeSerializeString = @"1!#!2!#!#!";
+            var sut = new Codec();
+
+            var expected = new TreeNode(1);
+            expected.right = new TreeNode(2);
+            //act
+            var actual = sut.deserialize(nodeSerializeString);
+
+            //assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void deserializeTest_三層節點()
+        {
+            //arrange
+            var nodeSerializeString = @"1!2!4!#!#!#!3!#!5!#!#!";
+            var sut = new Codec();
+
+
+            var expected = new TreeNode(1);
+            expected.left = new TreeNode(2);
+            expected.right = new TreeNode(3);
+
+            expected.left.left = new TreeNode(4);
+            expected.right.right = new TreeNode(5);
+
+            //act
+            var actual = sut.deserialize(nodeSerializeString);
+
+            //assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
