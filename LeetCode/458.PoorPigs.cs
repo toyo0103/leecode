e402224fdc2291b235a8183aac45177307029d0f
StevenTsai <steventsai@urtoday.com>
2018-06-19 16:36:26 +0800

    458.PoorPigs

---
+++ LeetCode/458.PoorPigs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+        {
+            var Pigs = 0;
+            if (buckets <= 1)
+            {
+                return Pigs;
+            }
+
+            var OnePigCanTestBukets = (minutesToTest / minutesToDie) + 1;
+            double TotalCanTestBukets = 0;
+            while (TotalCanTestBukets < buckets)
+            {
+                Pigs++;
+                TotalCanTestBukets = Math.Pow(OnePigCanTestBukets, Pigs);
+            }
+
+            return Pigs;
+        }
+    }
+}
---
+++ LeetCodeTests/458.PoorPigs.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+namespace LeetCode.Tests
+{
+    public partial class SolutionTests
+    {
+        [TestMethod]
+        public void PoorPigsTest_5桶水_1小時_毒發時間15分鐘_最少需要1隻豬來測試()
+        {
+            //arrange
+            var sut = new Solution();
+            var bukets = 5;
+            var minutesToDie = 15;
+            var minutesToTest = 60;
+
+            var expected = 1;
+            //act
+            var actual = sut.PoorPigs(bukets, minutesToDie, minutesToTest);
+
+            //assert
+            actual.Should().Be(expected);
+
+        }
+
+        [TestMethod]
+        public void PoorPigsTest_6桶水_1小時_毒發時間15分鐘_最少需要2隻豬來測試()
+        {
+            //arrange
+            var sut = new Solution();
+            var bukets = 6;
+            var minutesToDie = 15;
+            var minutesToTest = 60;
+
+            var expected = 2;
+            //act
+            var actual = sut.PoorPigs(bukets, minutesToDie, minutesToTest);
+
+            //assert
+            actual.Should().Be(expected);
+
+        }
+
+        [TestMethod]
+        public void PoorPigsTest_1桶水_1分鐘測試時間_毒發時間1分鐘_最少需要0隻豬來測試()
+        {
+            //arrange
+            var sut = new Solution();
+            var bukets = 1;
+            var minutesToDie = 1;
+            var minutesToTest = 1;
+
+            var expected = 0;
+            //act
+            var actual = sut.PoorPigs(bukets, minutesToDie, minutesToTest);
+
+            //assert
+            actual.Should().Be(expected);
+
+        }
+    }
+}
