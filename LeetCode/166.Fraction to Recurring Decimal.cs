9cf6bd5628acb80dd86cd482be6312f983fbf430
StevenTsai <steventsai@urtoday.com>
2018-06-27 17:21:06 +0800

    【LeetCode】Fraction to Recurring Decimal

---
+++ LeetCode/166.Fraction to Recurring Decimal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public string FractionToDecimal(int numerator, int denominator)
+        {
+            if (numerator == 0)
+            {
+                return "0";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            //考慮負數
+            sb.Append((numerator >= 0) ^ (denominator >= 0) ? "-" : string.Empty);
+
+            //只取正數
+            var _numerator =Math.Abs(Convert.ToInt64(numerator));
+            var _denominator = Math.Abs(Convert.ToInt64(denominator));
+            sb.Append(_numerator / _denominator);
+
+            //餘數
+            var Remainder = (_numerator % _denominator);
+            if (Remainder == 0)
+            {
+                return sb.ToString();
+            }
+
+            sb.Append(".");
+            var Map = new Dictionary<Int64, int>();
+            Map.Add(Remainder, sb.Length);
+            while (Remainder != 0)
+            {
+                sb.Append((Remainder *10) / _denominator);
+                Remainder = (Remainder * 10) % _denominator;
+                if (Map.ContainsKey(Remainder))
+                {
+                    var Index = Map[Remainder];
+                    //開始重複
+                    sb.Insert(Index, "(");
+                    sb.Append(")");
+                    break;
+                }
+
+                Map.Add(Remainder, sb.Length);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ LeetCodeTests/166.Fraction to Recurring Decimal.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+namespace LeetCode.Tests
+{
+    public partial class SolutionTests
+    {
+        [TestMethod]
+        public void FractionToDecimalTest_輸入numerator_1_denominator_2()
+        {
+            //arrange
+            var sut = new Solution();
+            var numerator = 1;
+            var denominator = 2;
+
+            var expected = "0.5";
+            //act
+            var actual = sut.FractionToDecimal(numerator, denominator);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void FractionToDecimalTest_輸入numerator_2_denominator_1()
+        {
+            //arrange
+            var sut = new Solution();
+            var numerator = 2;
+            var denominator = 1;
+
+            var expected = "2";
+            //act
+            var actual = sut.FractionToDecimal(numerator, denominator);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void FractionToDecimalTest_輸入numerator_2_denominator_3()
+        {
+            //arrange
+            var sut = new Solution();
+            var numerator = 2;
+            var denominator = 3;
+
+            var expected = "0.(6)";
+            //act
+            var actual = sut.FractionToDecimal(numerator, denominator);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void FractionToDecimalTest_輸入numerator_負2147483648_denominator_負1()
+        {
+            //arrange
+            var sut = new Solution();
+            var numerator = -2147483648;
+            var denominator = -1;
+
+            var expected = "2147483648";
+            //act
+            var actual = sut.FractionToDecimal(numerator, denominator);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void FractionToDecimalTest_輸入numerator_負50_denominator_8()
+        {
+            //arrange
+            var sut = new Solution();
+            var numerator = -50;
+            var denominator = 8;
+
+            var expected = "-6.25";
+            //act
+            var actual = sut.FractionToDecimal(numerator, denominator);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void FractionToDecimalTest_輸入numerator_0_denominator_負5()
+        {
+            //arrange
+            var sut = new Solution();
+            var numerator = 0;
+            var denominator = -5;
+
+            var expected = "0";
+            //act
+            var actual = sut.FractionToDecimal(numerator, denominator);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void FractionToDecimalTest_輸入numerator_7_denominator_負12()
+        {
+            //arrange
+            var sut = new Solution();
+            var numerator = 7;
+            var denominator = -12;
+
+            var expected = "-0.58(3)";
+            //act
+            var actual = sut.FractionToDecimal(numerator, denominator);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+    }
+}
